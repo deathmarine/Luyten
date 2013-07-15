06d6eee9010d6b08bb1a5bb7db405fa0585cc86a
Mike Strobel <mike.strobel@gmail.com>
2013-07-14 22:28:04 -0400

    JAR-scoped ITypeLoader and MetadataSystem instances for better performance.  Added debug languages menu.  Added standard type loader to make sure JRE types can be resolved.

---
+++ src/com/modcrafting/luyten/Closer.java
@@ -0,0 +1,23 @@
+package com.modcrafting.luyten;
+
+public final class Closer {
+    public static void tryClose(final AutoCloseable c) {
+        if (c == null) {
+            return;
+        }
+        try {
+            c.close();
+        }
+        catch (Throwable ignored) {
+        }
+    }
+
+    public static void tryClose(final AutoCloseable... items) {
+        if (items == null) {
+            return;
+        }
+        for (AutoCloseable c : items) {
+            tryClose(c);
+        }
+    }
+}
