a5565feeac541c76cabc24676792e4da591bd7fb
Quitten <quitten11@gmail.com>
2014-08-01 19:06:19 +0300

    findall feature

---
+++ src/com/modcrafting/luyten/FileIsBinaryException.java
@@ -0,0 +1,6 @@
+package com.modcrafting.luyten;
+
+public class FileIsBinaryException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/com/modcrafting/luyten/TooLargeFileException.java
@@ -0,0 +1,20 @@
+package com.modcrafting.luyten;
+
+import java.text.DecimalFormat;
+
+public class TooLargeFileException extends Exception {
+	private static final long serialVersionUID = 1L;
+	private long size;
+
+	public TooLargeFileException(long size) {
+		this.size = size;
+	}
+
+	public String getReadableFileSize() {
+		if (size <= 0)
+			return "0";
+		final String[] units = new String[] {"B", "KB", "MB", "GB", "TB"};
+		int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+		return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+	}
+}
