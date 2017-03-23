eeb20e4f07371df13805993e3b01dd846c7d5a37
FisheyLP <FisheyLP@gmail.com>
2017-03-23 16:33:34 +0100

    Added .yml file icon

--- src/us/deathmarine/luyten/CellRenderer.java
+++ src/us/deathmarine/luyten/CellRenderer.java
@@ -13,6 +13,7 @@ public class CellRenderer extends DefaultTreeCellRenderer {
 	private static final long serialVersionUID = -5691181006363313993L;
 	Icon pack;
 	Icon java_image;
+	Icon yml_image;
 	Icon file_image;
 
 	public CellRenderer() {
@@ -20,10 +21,12 @@ public class CellRenderer extends DefaultTreeCellRenderer {
 				Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/resources/package_obj.png")));
 		this.java_image = new ImageIcon(
 				Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/resources/java.png")));
+		this.yml_image = new ImageIcon(
+				Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/resources/yml.png")));
 		this.file_image = new ImageIcon(
 				Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/resources/file.png")));
 	}
-
+	
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf,
 			int row, boolean hasFocus) {
@@ -33,6 +36,8 @@ public class CellRenderer extends DefaultTreeCellRenderer {
 			setIcon(this.pack);
 		} else if (getFileName(node).endsWith(".class") || getFileName(node).endsWith(".java")) {
 			setIcon(this.java_image);
+		} else if (getFileName(node).endsWith(".yml") || getFileName(node).endsWith(".yaml")) {
+			setIcon(this.yml_image);
 		} else {
 			setIcon(this.file_image);
 		}
