617c5e1b7142a7f6a545e39214e2ddf78d8f26b7
Nico Mexis <nico.mexis@kabelmail.de>
2021-10-09 10:28:08 +0200

    Optimize imports

--- src/us/deathmarine/luyten/FileSaver.java
+++ src/us/deathmarine/luyten/FileSaver.java
@@ -1,7 +1,5 @@
 package us.deathmarine.luyten;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedOutputStream;
